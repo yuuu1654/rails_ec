3ff81965eb2aee7e2544ac8c63a4e88c00e4dc5d
morishitayuuki <morishita@heartorg.co.jp>
2023-11-11 15:11:56 +0900

    [Refactor] 不要コード削除

--- db/schema.rb
+++ db/schema.rb
@@ -50,14 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_05_075823) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.integer "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
