51aae73866491b0af88d7d140125834bc6e051c0
morishitayuuki <morishita@heartorg.co.jp>
2023-11-23 11:22:26 +0900

    [Refactor] format

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,64 +1,68 @@
-class Admin::ProductsController < ApplicationController
-  before_action :basic_auth
-  before_action :set_product, only: %i[show edit update destroy]
-  before_action :admin?, only: :index
+# frozen_string_literal: true
 
-  def index
-    @admin_products = Product.all
-  end
+module Admin
+  class ProductsController < ApplicationController
+    before_action :basic_auth
+    before_action :set_product, only: %i[show edit update destroy]
+    before_action :admin?, only: :index
 
-  def new
-    @product = Product.new
-  end
-
-  def show; end
-
-  def create
-    @product = Product.new(product_params)
-    @product.image.attach(params[:product][:image])
-    if @product.save
-      # flash[:success] = '商品を登録しました'
-      redirect_to admin_products_path, notice: '商品を登録しました'
-    else
-      render :new, status: :unprocessable_entity
+    def index
+      @admin_products = Product.all
     end
-  end
 
-  def edit; end
-
-  def update
-    if @product.update(product_params)
-      # flash[:success] = '商品情報を更新しました'
-      logger.debug 'koko1'
-      redirect_to admin_products_path
-    else
-      render :edit, status: :unprocessable_entity # status: :unprocessable_entity でエラーメッセージが表示されるようにする
+    def new
+      @product = Product.new
     end
-  end
 
-  def destroy
-    @product.destroy
-    flash[:success] = '商品を削除しました'
-    redirect_to admin_products_path, status: :see_other
-  end
+    def show; end
 
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == 'ps'
+    def create
+      @product = Product.new(product_params)
+      @product.image.attach(params[:product][:image])
+      if @product.save
+        # flash[:success] = '商品を登録しました'
+        redirect_to admin_products_path, notice: '商品を登録しました'
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
-  end
 
-  def set_product
-    @product = Product.find(params[:id])
-  end
+    def edit; end
 
-  def product_params
-    params.require(:product).permit(:name, :price, :description, :image)
-  end
+    def update
+      if @product.update(product_params)
+        # flash[:success] = '商品情報を更新しました'
+        logger.debug 'koko1'
+        redirect_to admin_products_path
+      else
+        render :edit, status: :unprocessable_entity # status: :unprocessable_entity でエラーメッセージが表示されるようにする
+      end
+    end
 
-  def admin?
-    @admin = true
+    def destroy
+      @product.destroy
+      flash[:success] = '商品を削除しました'
+      redirect_to admin_products_path, status: :see_other
+    end
+
+    private
+
+    def basic_auth
+      authenticate_or_request_with_http_basic do |username, password|
+        username == 'admin' && password == 'ps'
+      end
+    end
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :price, :description, :image)
+    end
+
+    def admin?
+      @admin = true
+    end
   end
 end
