66d379707ab9fb0362c4a3acf3ff504b538cf8ec
morishitayuuki <morishita@heartorg.co.jp>
2023-11-23 11:07:50 +0900

    [Update] 商品モデルの検証 追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@
 
 class Product < ApplicationRecord
   has_one_attached :image
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :image, attached: true, content_type: ['image/jpeg', 'image/gif', 'image/png']
 end
