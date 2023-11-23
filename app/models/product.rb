36bfec849da0922adffe6c64c504717c3c5b3eaa
morishitayuuki <morishita@heartorg.co.jp>
2023-11-23 11:43:24 +0900

    [Update] seedデータを流し込むため一旦コメントアウトにした

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,6 @@ class Product < ApplicationRecord
   has_one_attached :image
   validates :name, presence: true
   validates :description, presence: true
-  validates :image, attached: true, content_type: ['image/jpeg', 'image/gif', 'image/png']
+  # rails db:seed が実行できなかったので、一旦コメントアウト
+  # validates :image, attached: true, content_type: ['image/jpeg', 'image/gif', 'image/png']
 end
