68a394d7ac19b1a1f2cf8383a609adaf94834a83
morishitayuuki <morishita@heartorg.co.jp>
2023-11-26 21:28:48 +0900

    [Add] カート詳細画面 追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,24 @@ class ProductsController < ApplicationController
     @related_products = Product.all.order('created_at desc').limit(4)
   end
 
+  def show_cart
+    @cart_items = session[:cart] || []
+    logger.debug "cart_items: #{@cart_items.inspect}"
+    @cart_details = @cart_items.map do |item|
+      # 以下の書き方だとデータを取得できなかった。add_to_cartでシンボルをキーにして格納したはずやのに...
+      # product = Product.find(item[:product_id])
+      # total_price = product.price * item[:quantity]
+      product = Product.find(item['product_id'])
+      total_price = product.price * item['quantity']
+      { product:, quantity: item['quantity'], total_price: }
+    end
+    logger.debug "cart_details: #{@cart_details.inspect}"
+    # 今度は逆に以下の書き方だとデータにアクセスできなかった
+    # @billed_amount = @cart_details.sum { |item| item["total_price"] }
+    @billed_amount = @cart_details.sum { |item| item[:total_price] }
+    logger.debug "billed_amount: #{@billed_amount.inspect}"
+  end
+
   private
 
   def set_product
