26d0b6cdc26a61d1e84cabc3019b9e293ee511a9
morishitayuuki <morishita@heartorg.co.jp>
2023-12-02 16:13:00 +0900

    [Refactor] 現在のカート情報を取得するヘルパーメソッド 追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # コントローラ内で定義したメソッドをビューのテンプレート内でも使えるようにする
+  helper_method :current_cart
+
+  def current_cart
+    if session[:cart_id]
+      @cart = Cart.find(session[:cart_id])
+    else
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
+    @cart
+  end
 end
