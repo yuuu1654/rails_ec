bf8c8337c5995388be1488a21b86b082f2a914e6
morishitayuuki <morishita@heartorg.co.jp>
2023-12-16 22:57:11 +0900

    [Update] 購入明細メール 修正
    (レイアウト/タイトル/差出人)

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,6 +3,6 @@ class OrderMailer < ApplicationMailer
   def order_detail(order, order_details)
     @order = order
     @order_details = order_details
-    mail(to: @order.email, subject: "購入明細")
+    mail(to: @order.email, subject: "ご購入ありがとうございます")
   end
 end
