11dabd0bf0a8cfff3b1467e880537afa7395d001
morishitayuuki <morishita@heartorg.co.jp>
2023-12-16 16:38:57 +0900

    [Add] 購入明細のモデル・テーブル 追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_27_222246) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_09_144528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_27_222246) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_details", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.string "product_name"
+    t.integer "price"
+    t.integer "quantity"
+    t.integer "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_details_on_order_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.bigint "cart_id", null: false
     t.string "name"
@@ -83,5 +94,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_27_222246) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cart_products", "carts"
   add_foreign_key "cart_products", "products"
+  add_foreign_key "order_details", "orders"
   add_foreign_key "orders", "carts"
 end
