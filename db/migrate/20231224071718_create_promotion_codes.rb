a5951ce2c9a3e581fb71738e3926fc04d3ce13cd
morishitayuuki <morishita@heartorg.co.jp>
2023-12-31 16:14:51 +0900

    [Refactor] format

--- db/migrate/20231224071718_create_promotion_codes.rb
+++ db/migrate/20231224071718_create_promotion_codes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreatePromotionCodes < ActiveRecord::Migration[7.0]
   def change
     create_table :promotion_codes do |t|
--- db/migrate/20231227012336_add_default_false_to_used_in_promotion_codes.rb
+++ db/migrate/20231227012336_add_default_false_to_used_in_promotion_codes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddDefaultFalseToUsedInPromotionCodes < ActiveRecord::Migration[7.0]
   def change
     # デフォルト値をfalseに設定
