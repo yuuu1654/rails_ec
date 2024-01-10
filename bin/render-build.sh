ea7011dfae9eb019e61c330123b875cf59a981c2
morishitayuuki <morishita@heartorg.co.jp>
2024-01-10 17:14:43 +0900

    [Fix] ビルドコマンド処理順 修正 ver2

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,6 @@ set -o errexit # 途中でエラーが発生した時に残りのコマンドが
 bundle install
 rails assets:precompile
 rails assets:clean
-rails db:reset
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rails db:reset
 rails db:migrate
 rails db:seed
