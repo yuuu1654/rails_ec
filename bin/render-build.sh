57dc45866a27742141e96618fdd7a6411e46c949
morishitayuuki <morishita@heartorg.co.jp>
2024-01-10 16:35:56 +0900

    [Update] 本番環境のデータをリセットし、再度流し込む

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,10 @@
 # exit on error
 set -o errexit # 途中でエラーが発生した時に残りのコマンドが実行されるのを防ぐ
 
+
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
-bundle exec rake db:seed
+rails db:reset
+rails db:migrate
+rails db:seed
+rails assets:precompile
+rails assets:clean
