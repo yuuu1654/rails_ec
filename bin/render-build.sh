22eb720d87e8fa83f301e7e283227bac609dc665
morishitayuuki <morishita@heartorg.co.jp>
2024-01-10 19:11:54 +0900

    [Update] ビルドコマンドはRenderのコンソール画面から実行する

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,9 @@
 set -o errexit # 途中でエラーが発生した時に残りのコマンドが実行されるのを防ぐ
 
 
-bundle install
-rails assets:precompile
-rails assets:clean
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rails db:reset
-rails db:migrate
-rails db:seed
+# bundle install
+# rails assets:precompile
+# rails assets:clean
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rails db:reset
+# rails db:migrate
+# rails db:seed
