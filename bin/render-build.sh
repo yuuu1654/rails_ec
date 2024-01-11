d23d034ac1f583b634eb0264c6914a14e4df6801
morishitayuuki <morishita@heartorg.co.jp>
2024-01-12 07:50:55 +0900

    [Update] Renderビルドコマンド 修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,13 @@
 set -o errexit # 途中でエラーが発生した時に残りのコマンドが実行されるのを防ぐ
 
 
-# bundle install
-# rails assets:precompile
-# rails assets:clean
+bundle install
+rails assets:precompile
+rails assets:clean
 # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rails db:reset
-# rails db:migrate
+rails db:migrate
 # rails db:seed
+
+# Renderコンソールに設定用のコマンド▼
+# bundle install && rails assets:precompile && rails assets:clean && rails db:migrate && rails db:seed && rake promotion_code:generate
+# bundle install && rails assets:precompile && rails assets:clean && rails db:migrate
